0b99a0cda1c1f353355a73a2c13ef3aa64837b4f
jinyoung <danroom85@gmail.com>
2024-12-01 15:00:57 +0900

    ➕ kakao auth

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
+import KakaoProvider from "next-auth/providers/kakao";
 
 import { db } from "~/server/db";
 
@@ -54,6 +55,10 @@ export const authConfig = {
           response_type: "code",
         },
       },
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
     }),
   ],
   adapter: PrismaAdapter(db),
